4598eb3450197e19e7c0631d176111f9a8faff9b
shikhovtsevDS <mikro192@mail.ru>
2019-03-17 12:43:25 +0500

    Initial commit

---
+++ src/main/java/ru/shikhovtsev/videoShop/VideoShopApplication.java
@@ -0,0 +1,13 @@
+package ru.shikhovtsev.videoShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoShopApplication.class, args);
+	}
+
+}
