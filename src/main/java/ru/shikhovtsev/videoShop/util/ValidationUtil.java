74b65a6dfc9039e970a0f5d93fc3eaa3b89c892e
shikhovtsev <shikhovtsev97@gmail.com>
2019-03-26 14:03:26 +0500

    add Global exception handler

---
+++ src/main/java/ru/shikhovtsev/videoShop/util/ValidationUtil.java
@@ -0,0 +1,14 @@
+package ru.shikhovtsev.videoShop.util;
+
+public class ValidationUtil {
+
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while (null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
+    }
+}
