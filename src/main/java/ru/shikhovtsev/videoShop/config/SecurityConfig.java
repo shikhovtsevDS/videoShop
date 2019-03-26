2eb3e353661b541fec6d2c0f61ea614b51f9aeba
shikhovtsev <shikhovtsev97@gmail.com>
2019-03-26 13:03:11 +0500

    fix login

--- src/main/java/ru/shikhovtsev/videoShop/config/SecurityConfig.java
+++ src/main/java/ru/shikhovtsev/videoShop/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package ru.shikhovtsev.videoShop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import ru.shikhovtsev.videoShop.service.UserService;
 
 @Configuration
@@ -17,35 +19,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Autowired
     public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userService);
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().permitAll();
 
-        http.csrf()
-                .disable()
-                .authorizeRequests()
-                .antMatchers("/resources/**", "/**").permitAll()
-                .anyRequest().permitAll()
-                .and();
+        http.csrf().disable();
 
         http.formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/spring_security_check")
+                //FIXME change to Profile.jsp
+                .defaultSuccessUrl("/products")
                 .failureUrl("/login?error")
                 .usernameParameter("username")
                 .passwordParameter("password")
-                .permitAll();
-
-        http.logout()
                 .permitAll()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout")
-                .invalidateHttpSession(true);
-
+                //For logout page
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/login?logout");
     }
 }
