7ce61801a16c79b8bed44ebd0f080ce930de523a
shikhovtsev <shikhovtsev97@gmail.com>
2019-03-26 12:57:14 +0500

    new db init

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS orders_products;
 DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -42,7 +43,15 @@ CREATE TABLE orders_products
 (
   order_id      INTEGER NOT NULL,
   product_id    INTEGER NOT NULL,
-  CONSTRAINT user_roles_idx UNIQUE (order_id, product_id),
+  CONSTRAINT order_products_idx UNIQUE (order_id, product_id),
   FOREIGN KEY (order_id) REFERENCES orders (id),
   FOREIGN KEY (product_id) REFERENCES products (id)
+);
+
+CREATE TABLE user_roles
+(
+  user_id INTEGER NOT NULL,
+  role    VARCHAR,
+  CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
