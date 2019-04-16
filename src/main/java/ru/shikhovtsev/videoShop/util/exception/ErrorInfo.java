24e54f31ea7375771a89ce96f00e21d5ec0ccc6a
shikhovtsev <shikhovtsev97@gmail.com>
2019-04-16 21:48:23 +0500

    added json exceptions

---
+++ src/main/java/ru/shikhovtsev/videoShop/util/exception/ErrorInfo.java
@@ -0,0 +1,16 @@
+package ru.shikhovtsev.videoShop.util.exception;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorInfo {
+    private final String url;
+    private final ErrorType type;
+    private final String[] details;
+
+    public ErrorInfo(CharSequence url, ErrorType type, String... details) {
+        this.url = url.toString();
+        this.type = type;
+        this.details = details;
+    }
+}
---
+++ src/main/java/ru/shikhovtsev/videoShop/web/ExceptionInfoHandler.java
@@ -0,0 +1,72 @@
+package ru.shikhovtsev.videoShop.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import ru.shikhovtsev.videoShop.util.ValidationUtil;
+import ru.shikhovtsev.videoShop.util.exception.ErrorInfo;
+import ru.shikhovtsev.videoShop.util.exception.ErrorType;
+import ru.shikhovtsev.videoShop.util.exception.NotFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static ru.shikhovtsev.videoShop.util.exception.ErrorType.*;
+
+@RestControllerAdvice(annotations = RestController.class)
+@Order(Ordered.HIGHEST_PRECEDENCE + 5)
+public class ExceptionInfoHandler {
+    private static Logger log = LoggerFactory.getLogger(ExceptionInfoHandler.class);
+
+    //  http://stackoverflow.com/a/22358422/548473
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler(NotFoundException.class)
+    public ErrorInfo handleError(HttpServletRequest req, NotFoundException e) {
+        return logAndGetErrorInfo(req, e, false, DATA_NOT_FOUND);
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
+    @ExceptionHandler({BindException.class, MethodArgumentNotValidException.class})
+    public ErrorInfo bindingError(HttpServletRequest req, Exception e) {
+        BindingResult result = e instanceof BindException ?
+                ((BindException) e).getBindingResult() : ((MethodArgumentNotValidException) e).getBindingResult();
+
+        String[] details = result.getFieldErrors().stream()
+                .map(fe -> {
+                    String msg = fe.getDefaultMessage();
+                    return (msg.startsWith(fe.getField())) ? msg : fe.getField() + ' ' + msg;
+                })
+                .toArray(String[]::new);
+
+        return logAndGetErrorInfo(req, e, false, VALIDATION_ERROR, details);
+    }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)  // 409
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ErrorInfo conflict(HttpServletRequest req, DataIntegrityViolationException e) {
+        return logAndGetErrorInfo(req, e, true, DATA_ERROR);
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorInfo handleError(HttpServletRequest req, Exception e) {
+        return logAndGetErrorInfo(req, e, true, APP_ERROR);
+    }
+
+    private static ErrorInfo logAndGetErrorInfo(HttpServletRequest req, Exception e, boolean logException, ErrorType errorType, String... details) {
+        Throwable rootCause = ValidationUtil.getRootCause(e);
+        if (logException) {
+            log.error(errorType + " at request " + req.getRequestURL(), rootCause);
+        } else {
+            log.warn("{} at request  {}: {}", errorType, req.getRequestURL(), rootCause.toString());
+        }
+        return new ErrorInfo(req.getRequestURL(), errorType, details.length != 0 ? details : new String[]{rootCause.toString()});
+    }
+}
